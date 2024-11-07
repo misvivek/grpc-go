b81a4e8cce197188d1fe5b8e26cd918202bcd857
misvivek <misvivek@google.com>
2024-11-07 21:29:30 +0000

    added the test in the transport_test.go

--- internal/transport/http2_server.go
+++ internal/transport/http2_server.go
@@ -1041,11 +1041,8 @@ func (t *http2Server) writeHeaderLocked(s *Stream) error {
 	}
 	return nil
 }
-func SignalDeadlineExceededForTesting(rst bool, st *status.Status) {
-	if st.Code() == codes.DeadlineExceeded {
-		rst = true
-	}
-}
+
+var signalDeadlineExceeded = func() {}
 
 // WriteStatus sends stream status to the client and terminates the stream.
 // There is no further I/O operations being able to perform on this stream.
@@ -1109,8 +1106,9 @@ func (t *http2Server) WriteStatus(s *Stream, st *status.Status) error {
 	}
 	// Send a RST_STREAM after the trailers if the client has not already half-closed.
 	rst := s.getState() == streamActive
-
-	SignalDeadlineExceededForTesting(rst, st)
+	if rst {
+		signalDeadlineExceeded()
+	}
 	t.finishStream(s, rst, http2.ErrCodeNo, trailingHeader, true)
 	for _, sh := range t.stats {
 		// Note: The trailer fields are compressed with hpack after this call returns.
